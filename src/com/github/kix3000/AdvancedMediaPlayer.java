6b8eabd91dab5dffce40e4a8b0dea5e7361fbcda
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-14 21:40:51 +0200

    Implemented Adapter Pattern.

---
+++ src/com/github/kix3000/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.github.kix3000;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ src/com/github/kix3000/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.github.kix3000;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
