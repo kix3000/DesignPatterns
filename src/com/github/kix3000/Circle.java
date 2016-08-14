aaf0b92c8ea34a1d0c5d7c2ad19b408634e2e89f
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-14 18:46:31 +0200

    Implemented Prototype Pattern.

---
+++ src/com/github/kix3000/Circle.java
@@ -0,0 +1,13 @@
+package com.github.kix3000;
+
+public class Circle extends Shape {
+
+    public Circle(){
+        type = "Circle";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle::draw() method.");
+    }
+}
