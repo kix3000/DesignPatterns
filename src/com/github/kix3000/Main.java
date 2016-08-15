d7d107afb77c35836baa9616f8857dbd0404d69f
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-15 14:32:59 +0200

    Implemented Composite Pattern.

--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -3,6 +3,36 @@ package com.github.kix3000;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting main...");
+        System.out.println("Starting main...\n");
+
+        Employee ceo = new Employee("John","CEO", 30000);
+
+        Employee headSales = new Employee("Robert","Head Sales", 20000);
+        Employee headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+        Employee clerk1 = new Employee("Laura","Marketing", 10000);
+        Employee clerk2 = new Employee("Bob","Marketing", 10000);
+
+        Employee salesExecutive1 = new Employee("Richard","Sales", 10000);
+        Employee salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+        ceo.add(headSales);
+        ceo.add(headMarketing);
+
+        headSales.add(salesExecutive1);
+        headSales.add(salesExecutive2);
+
+        headMarketing.add(clerk1);
+        headMarketing.add(clerk2);
+
+        System.out.println(ceo);
+
+        for (Employee headEmployee : ceo.getSubordinates()) {
+            System.out.println(headEmployee);
+
+            for (Employee employee : headEmployee.getSubordinates()) {
+                System.out.println(employee);
+            }
+        }
     }
 }
