69c425f009310b722804f55abd5ad5ce91aa333c
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-15 14:59:29 +0200

    Implemented Proxy Pattern.

---
+++ src/com/github/kix3000/Image.java
@@ -0,0 +1,5 @@
+package com.github.kix3000;
+
+public interface Image {
+    void display();
+}
--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -3,6 +3,13 @@ package com.github.kix3000;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting main...");
+        System.out.println("Starting main...\n");
+
+        Image image = new ProxyImage("test_10mb.jpg");
+
+        image.display();
+        System.out.println();
+
+        image.display();
     }
 }
