203cabfcb2f04b40da3c72ed10e600566154d9a9
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-15 14:43:33 +0200

    Implemented Facade Pattern.

--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -3,6 +3,13 @@ package com.github.kix3000;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting main...");
+        System.out.println("Starting main...\n");
+
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+
     }
 }
---
+++ src/com/github/kix3000/Rectangle.java
@@ -0,0 +1,9 @@
+package com.github.kix3000;
+
+public class Rectangle implements Shape {
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle::draw()");
+    }
+}
