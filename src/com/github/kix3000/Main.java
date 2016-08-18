9659ad077a9f26c5885e707641d073401a579c89
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-18 23:37:35 +0200

    Implemented Chain of Responsibility Pattern.

--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -3,6 +3,29 @@ package com.github.kix3000;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting main...");
+        System.out.println("Starting main...\n");
+
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        loggerChain.logMessage(AbstractLogger.INFO,
+                "This is an information.");
+
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is an debug level information.");
+
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.");
+    }
+
+    private static AbstractLogger getChainOfLoggers(){
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
     }
 }
