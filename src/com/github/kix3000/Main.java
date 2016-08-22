6609fd634ba157b5e7d1a0a6c72b2e215da70f60
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-08-22 21:59:14 +0200

    Implemented Interpreter Pattern.

--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -3,6 +3,26 @@ package com.github.kix3000;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting main...");
+        System.out.println("Starting main...\n");
+
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+    }
+
+    //Rule: Robert and John are male
+    public static Expression getMaleExpression(){
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    //Rule: Julie is a married women
+    public static Expression getMarriedWomanExpression(){
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+        return new AndExpression(julie, married);
     }
 }
