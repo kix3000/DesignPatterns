3449d5d9c54d140eb1a9d1948effe4a4706f0387
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-09-25 15:19:54 +0200

    Implemented Memento Pattern.

---
+++ src/com/github/kix3000/Memento.java
@@ -0,0 +1,13 @@
+package com.github.kix3000;
+
+public class Memento {
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+}
