ca746c028e11abb201dcf585da7498e6d80008c9
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-09-25 15:10:00 +0200

    Implemented Iterator Pattern.

---
+++ src/com/github/kix3000/Container.java
@@ -0,0 +1,5 @@
+package com.github.kix3000;
+
+public interface Container {
+    public Iterator getIterator();
+}
---
+++ src/com/github/kix3000/Iterator.java
@@ -0,0 +1,6 @@
+package com.github.kix3000;
+
+public interface Iterator {
+    public boolean hasNext();
+    public Object next();
+}
