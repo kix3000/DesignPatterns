5517142be8651e5f0009cc31927bf414309c2120
Michal Chojnacki <michal.chojnacki@snow.dog>
2016-09-25 15:23:05 +0200

    Implemented Null Object Pattern.

---
+++ src/com/github/kix3000/AbstractCustomer.java
@@ -0,0 +1,7 @@
+package com.github.kix3000;
+
+public abstract class AbstractCustomer {
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
--- src/com/github/kix3000/Main.java
+++ src/com/github/kix3000/Main.java
@@ -5,22 +5,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Starting main...\n");
 
-        Originator originator = new Originator();
-        CareTaker careTaker = new CareTaker();
+        AbstractCustomer customer1 = CustomerFactory.getCustomer("Rob");
+        AbstractCustomer customer2 = CustomerFactory.getCustomer("Bob");
+        AbstractCustomer customer3 = CustomerFactory.getCustomer("Julie");
+        AbstractCustomer customer4 = CustomerFactory.getCustomer("Laura");
 
-        originator.setState("State #1");
-        originator.setState("State #2");
-        careTaker.add(originator.saveStateToMemento());
-
-        originator.setState("State #3");
-        careTaker.add(originator.saveStateToMemento());
-
-        originator.setState("State #4");
-        System.out.println("Current State: " + originator.getState());
-
-        originator.getStateFromMemento(careTaker.get(0));
-        System.out.println("First saved State: " + originator.getState());
-        originator.getStateFromMemento(careTaker.get(1));
-        System.out.println("Second saved State: " + originator.getState());
+        System.out.println("Customers");
+        System.out.println(customer1.getName());
+        System.out.println(customer2.getName());
+        System.out.println(customer3.getName());
+        System.out.println(customer4.getName());
     }
 }
